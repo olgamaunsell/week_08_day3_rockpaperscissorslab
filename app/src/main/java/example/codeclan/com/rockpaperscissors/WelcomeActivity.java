6e6a29d8c21967500230bc66340bfa43303003ae
Olga Maunsell <olgamaunsell@yahoo.co.uk>
2018-01-26 11:56:53 +0000

    exercise to practice creating a menu

--- app/src/main/java/example/codeclan/com/rockpaperscissors/WelcomeActivity.java
+++ app/src/main/java/example/codeclan/com/rockpaperscissors/WelcomeActivity.java
@@ -3,8 +3,12 @@ package example.codeclan.com.rockpaperscissors;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -16,9 +20,36 @@ public class WelcomeActivity extends AppCompatActivity {
         play = findViewById(R.id.playBtn);
     }
 
-    public void onPlayButtonClick(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_hello) {
+            Toast.makeText(WelcomeActivity.this, R.string.menu_toast_hello,
+                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (item.getItemId() == R.id.action_play) {
+            beginGame();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void beginGame() {
         Intent beginGame = new Intent(this, GameActivity.class);
         startActivity(beginGame);
+
+    }
+
+    public void onPlayButtonClick(View view) {
+        beginGame();
 
     }
 }
